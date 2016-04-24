b44e1f533d3ccb8b55f9f5722f89a0861a926e13
Todd <pollaktodd@gmail.com>
2016-04-24 11:13:09 -0400

    trying expire again

--- src/redis_scripts/interest_add.lua
+++ src/redis_scripts/interest_add.lua
@@ -1,10 +1,10 @@
 --Script to add interests
 if redis.call('EXISTS', KEYS[1]) == 1 then
 	redis.call('SADD', KEYS[1], ARGV[1])
-	-- redis.call('EXPIRE', [KEYS[1], 15]) --Age the interest
+	redis.call('EXPIRE', [KEYS[1], 8]) --Age the interest
 	return 1
 else
 	redis.call('SADD', KEYS[1], ARGV[1])
-	-- redis.call('EXPIRE', [KEYS[1], 15])
+	redis.call('EXPIRE', [KEYS[1], 8])
 	return 0
 end
