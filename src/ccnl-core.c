95fae9a2d6f2a7f0e4d34692b45bb4c10369c5e8
Todd <pollaktodd@gmail.com>
2016-04-24 10:23:51 -0400

    remove aged faces

--- src/ccnl-core.c
+++ src/ccnl-core.c
@@ -523,7 +523,9 @@ ccnl_interest_remove(struct ccnl_relay_s *ccnl, struct ccnl_interest_s *i) /* TO
 /*
     if (!i)
         return NULL;
-*/
+*/  
+    char *content_n = ccnl_prefix_to_path(i->pkt->pfx);
+    redisReply *reply = redisCommand(relay->redis_content,"Del i-%b", content_n, strlen(content_n));
     DEBUGMSG_CORE(TRACE, "ccnl_interest_remove %p\n", (void *) i);
 
 /*
